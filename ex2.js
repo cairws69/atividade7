3ca8476a5af11b48bf1c01ce442afa0b1ac5dc22
cairws69 <143729651+cairws69@users.noreply.github.com>
2023-10-03 11:18:36 -0300

    Add files via upload

---
+++ ex2.js
@@ -0,0 +1,26 @@
+class Pessoa {
+    constructor(nome, telefone, renda ,email){
+        this._nome = nome;
+        this._telefone = telefone;
+        this._renda = renda;
+        this._email = email;
+    }
+    verificarCredito(){
+         return new Promise ((resolve, reject) => {
+            if(this._renda > 2000){
+                resolve(`Credito aprovado`)
+            } else{
+                reject(`Credito recusado`)
+            }
+         })
+    }
+}
+    const cliente1 =new Pessoa("Joao Pedro","19971660755","100","jpedrocaires@gmail.com")
+    cliente1.verificarCredito()
+    .then((mensagem) =>{
+    console.log(mensagem)
+    })
+    .catch((erro) =>{
+    console.error(erro)
+    }
+    )
